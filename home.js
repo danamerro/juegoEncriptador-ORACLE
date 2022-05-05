70131eeaafa733c5a113d1650a5e123ac71fd819
danamerro <danamerro@gmail.com>
2022-05-05 01:19:07 -0300

    ultimos campios del challenge

--- home.js
+++ home.js
@@ -6,3 +6,22 @@ function desaparecerDiv(){
     
 
 }
+function encriptarTexto(){
+    //var textoUsuario= document.getElementById("inputEncriptar");
+    //
+    var textoUsuario= document.getElementById("inputEncriptar");
+    var palabraCodificada= btoa(textoUsuario.value);
+    document.getElementById("pasajeDeMensaje").innerHTML=palabraCodificada;
+}
+function desencriptar(){
+    var textoUsuario= document.getElementById("inputEncriptar");
+    var palabraCodificada= atob(textoUsuario.value);
+    document.getElementById("pasajeDeMensaje").innerHTML=palabraCodificada;
+}
+function copiarTexto(){
+    var content= document.getElementById("pasajeDeMensaje").innerHTML;
+    navigator.clipboard.writeText(content);
+    
+}
+
+
